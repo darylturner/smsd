81f90550eb91b8d6026e339f5d8b63a0964863e7
Daryl Turner <daryl@layer-eight.uk>
2016-07-23 22:05:29 +0100

    changed incoming data handling to use internal event emitter.

--- libsms.js
+++ libsms.js
@@ -1,9 +1,11 @@
-var serialport = require('serialport');
-var bunyan = require('bunyan');
+const serialport = require('serialport');
+const bunyan = require('bunyan');
+const EventEmitter = require('events');
 
-var modem = {
+const modem = {
     create: function(options) {
         var instance = Object.create(this);
+        instance.emitter = new EventEmitter;
         instance.options = options;
         instance.options.autoOpen = false;
         instance.locked = false;
@@ -16,17 +18,18 @@ var modem = {
     connect: function(callback) {
         this.port = new serialport(this.options.device, this.options);
         this.log.info('opening serial port');
-        this.port.open((err) => {
+        this.port.open(err => {
             this.log.info('port opened');
             if (!err) {
-                this.port.flush((err) => {
+                this.port.flush(err => {
                     if (err) {
                         this.log.error(err, 'error flushing data');
                         return callback(err);
                     }
+                    this.startDataHandler();
                     this.log.info('setting up modem')
-                    this.port.write('ATE0;+CMGF=1\r', (err) => {
-                        this.wait(callback);
+                    this.port.write('ATE0;+CMGF=1\r', err => {
+                        this.emitter.once('termination', callback);
                     });
                 });
             } else {
@@ -43,18 +46,18 @@ var modem = {
         this.log.info('querying modem signal strength')
         var cmd = 'AT+CSQ\r';
         this.log.debug({sent: cmd});
-        this.port.write(cmd, (err) => {
+        this.port.write(cmd, err => {
             if (err) {
                 this.log.error(err);
                 return callback(err);
             }
-            this.wait((err, reply) => {
-                if (!err) { //parse reply from modem
+            this.emitter.once('termination', (err, reply) => {
+                if (!err) {
                     this.log.info('received signal information')
-                    var result = reply.split('\n')[1]
+                    const result = reply.split('\n')[1]
                                  .split(':')[1].trim().split(',');
-                    var rssi = parseInt(result.shift(), 10);
-                    var ber = parseInt(result.shift(), 10);
+                    const rssi = parseInt(result.shift(), 10);
+                    const ber = parseInt(result.shift(), 10);
                     var status
                     if (rssi < 11) {
                         status = 'weak';
@@ -81,47 +84,44 @@ var modem = {
         }
         this.locked = true;
         this.log.info({sms: sms}, 'attempting to send message');
-        var cmd = `AT+CMGS=${sms.recipient}\r`;
+        const cmd = `AT+CMGS=${sms.recipient}\r`;
         this.log.debug({sent: cmd});
-        this.port.write(cmd, (err) => {
+        this.port.write(cmd, err => {
             if (err) {
                 this.log.error(err, 'error on message write')
+                this.locked = false;
                 return callback(err);
             }
-            this.wait(() => {
-                var cmd = `${sms.message}${String.fromCharCode(26)}`;
+            this.emitter.once('continue', () => {
+                const cmd = `${sms.message}${String.fromCharCode(26)}`;
                 this.log.debug({sent: cmd});
                 this.port.write(cmd, () => {
-                    this.wait(callback);
+                    this.emitter.once('termination', callback);
                 });
-            });
+            })
         });
     },
     close: function(callback) {
         this.port.close(callback);
     },
-    wait: function(callback) {
-        this.log.info('waiting for modem response')
+    startDataHandler: function() {
         var buffer = '';
-        var self = this; // need a named function here so need a scoped this.
-        this.port.on('data', function parser(chunk) {
-            self.log.debug({received: chunk.toString()})
+        this.port.on('data', chunk => {
+            this.log.debug({received: chunk.toString()})
             buffer += chunk;
-            // search for message delimiter
-            if (buffer.search(/OK|ERROR|>/) !== -1) {
-                var err;
-                if (buffer.search('ERROR') !== -1) {
-                    err = new Error('modem error')
-                    self.log.error(err);
-                }
-                if (buffer.match('>')) {
-                    // don't release lock.
-                    self.port.removeListener('data', parser);
-                    return callback();
-                }
-                self.port.removeListener('data', parser);
-                self.locked = false;
-                return callback(err, buffer);
+            if (buffer.search('>') !== -1) {
+                this.emitter.emit('continue', null, null);
+                buffer = '';
+            } else if (buffer.search('ERROR') !== -1) {
+                err = new Error('modem error');
+                this.log.error(err);
+                this.emitter.emit('termination', err, buffer);
+                buffer = '';
+                this.locked = false;
+            } else if (buffer.search('OK') !== -1) {
+                this.emitter.emit('termination', null, buffer);
+                buffer = '';
+                this.locked = false;
             }
         });
     }
